40f31714ae6d61757bb0e469c4c299b7a65cafa1
bartek <smalecbartlomiej@gmail.com>
2022-12-19 22:09:21 +0100

    Added stripe payment processing backend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/bartek/ecommerce/service/CheckoutService.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/bartek/ecommerce/service/CheckoutService.java
@@ -1,8 +1,12 @@
 package com.bartek.ecommerce.service;
 
+import com.bartek.ecommerce.dto.PaymentInfo;
 import com.bartek.ecommerce.dto.Purchase;
 import com.bartek.ecommerce.dto.PurchaseResponse;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
+    PaymentIntent createPaymentInfo(PaymentInfo paymentInfo) throws StripeException;
 }
